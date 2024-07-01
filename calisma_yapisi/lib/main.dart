cb01cf17c0abbf097af98dc18b39f65790fa03eb
AsudeAgaya0 <asudeagaya@gmail.com>
2024-07-02 00:32:48 +0300

    Update main.dart

--- calisma_yapisi/lib/main.dart
+++ calisma_yapisi/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const KullaniciEtkilesimiSayfa(),//ilk çalışacak olan sayfa anasayfa, buraya bağlanmış oldu
+      home: const Anasayfa(),//ilk çalışacak olan sayfa anasayfa, buraya bağlanmış oldu
     );
   }
 }
