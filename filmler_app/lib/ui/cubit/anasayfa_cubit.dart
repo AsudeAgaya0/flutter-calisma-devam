68b25ca9c7d209fe3c8a456f63f67f0c243b9e32
AsudeAgaya0 <81038103+AsudeAgaya0@users.noreply.github.com>
2024-08-19 12:35:46 +0300

    cubit kullanimi

--- filmler_app/lib/ui/cubit/anasayfa_cubit.dart
+++ filmler_app/lib/ui/cubit/anasayfa_cubit.dart
@@ -3,12 +3,16 @@ import 'package:filmler_app/data/repo/filmlerdao_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AnasayfaCubit extends Cubit<List<Filmler>> {
-  AnasayfaCubit() : super(<Filmler>[]);
+  //sayfama hangi türde geri dönüş yapılmasını istiyorsam o türde yazıyorum
+  AnasayfaCubit()
+      : super(
+            <Filmler>[]); //super içi cubit türüyle aynı olmalı, eğer arayüze veri gönderilmeyecekse void tanımlaması yapılıp super(0) yazılabilir, 0'ın herhangi bir etkisi olmayacak
 
   var frepo = FilmlerDaoRepository();
 
   Future<void> filmleriYukle() async {
     var liste = await frepo.filmleriYukle();
-    emit(liste);
+    emit(
+        liste); //emit:tetiklemek(onu dinleyen bir yer varsa tetiklemek için),anasayfaya veri göndermek için kullanıyoruz, kullanacağımız yerde de onu dinliyoruz
   }
 }
--- filmler_app/lib/ui/views/anasayfa.dart
+++ filmler_app/lib/ui/views/anasayfa.dart
@@ -1,6 +1,8 @@
 import 'package:filmler_app/data/entity/filmler.dart';
+import 'package:filmler_app/ui/cubit/anasayfa_cubit.dart';
 import 'package:filmler_app/ui/views/detay_sayfa.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
@@ -10,48 +12,46 @@ class Anasayfa extends StatefulWidget {
 }
 
 class _AnasayfaState extends State<Anasayfa> {
-
-  Future<List<Filmler>> filmleriYukle() async {
-    var filmListesi = <Filmler>[];
-    var f1 = Filmler(id: 1, ad: "Django", resim: "django.png", fiyat: 10);
-    var f2 = Filmler(id: 2, ad: "Interstellar", resim: "interstellar.png", fiyat: 20);
-    var f3 = Filmler(id: 3, ad: "Inception", resim: "inception.png", fiyat: 30);
-    var f4 = Filmler(id: 4, ad: "The Hateful Eigth", resim: "thehatefuleight.png", fiyat: 40);
-    var f5 = Filmler(id: 5, ad: "The Pianist", resim: "thepianist.png", fiyat: 50);
-    var f6 = Filmler(id: 6, ad: "Anadoluda", resim: "anadoluda.png", fiyat: 60);
-
-    filmListesi.add(f1);
-    filmListesi.add(f2);
-    filmListesi.add(f3);
-    filmListesi.add(f4);
-    filmListesi.add(f5);
-    filmListesi.add(f6);
-
-    return filmListesi;
+  @override
+  void initState() {
+    //uygulama yüklenir yüklenmez görmek istediğim için buraya ekledim
+    super.initState();
+    context.read<AnasayfaCubit>().filmleriYukle();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Filmler"),),
-      body: FutureBuilder<List<Filmler>>(
-        future: filmleriYukle(),
-        builder: (context, snapshot){ // snapshot bize verileri getirecek
-          if(snapshot.hasData){
-            var filmlerlistesi = snapshot.data;
+      appBar: AppBar(
+        title: const Text("Filmler"),
+      ),
+      body: BlocBuilder<AnasayfaCubit, List<Filmler>>(
+        //cubit içinden emit edileni dinlemek için blocbuilder kullanıldı
+        //hangi cubitten veri geleceği ve tipinin ne olacağı eklenir
+        builder: (context, filmlerListesi) {
+          // snapshot bize verileri getirecek
+          if (filmlerListesi.isNotEmpty) {
             return GridView.builder(
-              itemCount: filmlerlistesi!.length,//kaç tane film eklenmişse listeye onu verecek
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount( //bir satırda kaç tane nesne olacağını göstermek için
+              itemCount: filmlerListesi
+                  .length, //kaç tane film eklenmişse listeye onu verecek
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                //bir satırda kaç tane nesne olacağını göstermek için
                 crossAxisCount: 2,
-                childAspectRatio: 1/1.8, //oranlama demek, yatay 1 ise yükseklik 1.8 olsun diyor
+                childAspectRatio: 1 /
+                    1.8, //oranlama demek, yatay 1 ise yükseklik 1.8 olsun diyor
               ),
-              itemBuilder: (context, indeks){//itemcount kadar çalışacak
+              itemBuilder: (context, indeks) {
+                //itemcount kadar çalışacak
                 // 0 1 2 3 4 5
-                var film = filmlerlistesi[indeks];
+                var film = filmlerListesi[indeks];
                 return GestureDetector(
-                  onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => DetaySayfa(film: film)));},
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DetaySayfa(film: film)));
+                  },
                   child: Card(
-
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
@@ -59,25 +59,26 @@ class _AnasayfaState extends State<Anasayfa> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Text("${film.fiyat} ₺", style: TextStyle(fontSize: 24),),
-                            ElevatedButton(onPressed: (){
-                              print("${film.ad} sepete eklendi");
-                            }, child: const Text("Sepet"))
-                  
+                            Text(
+                              "${film.fiyat} ₺",
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  print("${film.ad} sepete eklendi");
+                                },
+                                child: const Text("Sepet"))
                           ],
                         )
                       ],
                     ),
                   ),
                 );
-
               },
             );
-
-          }else{
+          } else {
             return const Center();
           }
-
         },
       ),
     );
